3a3157b0cd89084320913803996a0a5bdd7f3667
Jonathan David Pelton <pelton.jonathand@gamil.com>
2019-11-16 13:37:51 -0800

    Added files

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.zoom').hover(function () {
+        $(this).addClass('transition');
+    }, function () {
+        $(this).removeClass('transition');
+    });
+});
+
+
+let gitHubRequest = new XMLHttpRequest();
+gitHubRequest.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let gitObject = JSON.parse(this.responseText);
+        document.getElementById("repos").innerHTML = gitObject[0].name;
+        document.getElementById("repos").innerHTML = gitObject[1].name;
+    }
+};
+gitHubRequest.open("GET", "https://api.github.com/users/WimpyWonder/repos", true);
+gitHubRequest.send();
